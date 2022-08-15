7e8855b541598f05c9b2a3987e2139c47ffc11db
Yurii Trihub <yuriitrihub@gmail.com>
2022-08-15 15:25:36 +0300

    реализация post.html добавление информации в случае отсутствии поста

---
+++ app/post/views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+from app.dao.main_dao import get_post_by_pk, get_comments_by_post_id
+
+post_blueprint = Blueprint('post_blueprint', __name__)
+
+
+@post_blueprint.route('/post/<post_id>', methods=["GET", "POST"])
+def post_page(post_id):
+    post_by_id = get_post_by_pk(post_id)
+    if not post_by_id:
+        return "<span class='item__username'>Пост отсутствует</span>"
+    else:
+        get_comments_by_post_id(post_id)
+        return render_template('post.html', post_by_id=post_by_id, comments=get_comments_by_post_id(post_id))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
-from app.main.views import main_blueprint
 
+from app.main.views import main_blueprint
+from app.post.views import post_blueprint
 from app.dao.main_dao import get_posts_all, get_post_by_pk
 
 
@@ -9,6 +10,7 @@ app.config['JSON_AS_ASCII'] = False
 
 
 app.register_blueprint(main_blueprint)
+app.register_blueprint(post_blueprint)
 
 if __name__ == '__main__':
     app.run()
