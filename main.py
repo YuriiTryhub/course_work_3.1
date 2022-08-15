e449e7f150d3eea282925411e58006237c331a79
Yurii Trihub <yuriitrihub@gmail.com>
2022-08-15 15:53:05 +0300

    реалзован user_feed.html в main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify
 from app.main.views import main_blueprint
 from app.post.views import post_blueprint
 from app.search.viewes import search_blueprint
+from app.user_feed.viewes import user_feed_blueprint
+
 from app.dao.main_dao import get_posts_all, get_post_by_pk
 
 
@@ -13,6 +15,8 @@ app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(main_blueprint)
 app.register_blueprint(post_blueprint)
 app.register_blueprint(search_blueprint)
+app.register_blueprint(user_feed_blueprint)
+
 
 if __name__ == '__main__':
     app.run()
