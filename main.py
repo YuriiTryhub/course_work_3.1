1231d9db40e9bc30d8dfa5674a7779e8da2b35e9
Yurii Trihub <yuriitrihub@gmail.com>
2022-08-15 16:03:46 +0300

    реализация 2 API - эндпоинта, исправление main_dao

--- main.py
+++ main.py
@@ -18,5 +18,16 @@ app.register_blueprint(search_blueprint)
 app.register_blueprint(user_feed_blueprint)
 
 
+@app.route('/api/posts')
+def get_posts():
+    posts = get_posts_all()
+    return jsonify(posts)
+
+
+@app.route('/api/posts/<int:post_id>')
+def get_post_id(post_id):
+    return get_post_by_pk(post_id)
+
+
 if __name__ == '__main__':
     app.run()
